d3fa847133abfe503d21741e6507f1a329fe9680
Kamil Pelinski <kpelinski@interia.pl>
2020-12-31 12:26:20 +0100

    add playermode

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+let computerMove = `kamień`;
+let playerMove = `papier`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' to wygrywasz!');
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
