4911c55d4f761f976850537cef32345c8a611af2
Kamil Pelinski <kpelinski@interia.pl>
2021-01-03 14:35:12 +0100

    add opt

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -14,3 +24,47 @@ if(randomNumber == 2){
 	computerMove = 'nożyce';
   }
 printMessage('Mój ruch to: ' + computerMove);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+if(playerInput == '2'){
+	playerMove = 'papier';
+}
+  if(playerInput == '3'){
+	playerMove = 'nożyce';
+  }
+printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+	printMessage('Ty wygrywasz!');
+  }
+  if( computerMove == 'papier' && playerMove == 'nożyce'){
+	printMessage('Ty wygrywasz!');
+  }
+  if( computerMove == 'nożyce' && playerMove == 'kamień'){
+	printMessage('Ty wygrywasz!');
+  }
+  if( computerMove == 'kamień' && playerMove == 'nożyce'){
+	printMessage('Komputer wygrywa!');
+  }
+  if( computerMove == 'papier' && playerMove == 'kamień'){
+	printMessage('Komputer wygrywa!');
+  }
+  if( computerMove == 'nożyce' && playerMove == 'papier'){
+	printMessage('Komputer wygrywa!');
+  }
+  if( computerMove == 'papier' && playerMove == 'papier'){
+	printMessage('Remis!');
+  }
+  if( computerMove == 'kamień' && playerMove == 'kamień'){
+	printMessage('Remis!');
+  }
+  if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+	printMessage('Remis!');
+  }
